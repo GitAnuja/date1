1f95cb789ca9825bd373e86acd4f36730ac59616
anuja.manoharan <anuja.manoharan@zohocorp.com>
2018-09-04 16:18:38 +0530

    first commit

---
+++ date.js
@@ -0,0 +1,13 @@
+var date = function date(){
+	this.closestToIndex = function(dateToCompare, datesArray){
+		var index = 0;
+		var min = Math.abs(dateToCompare-datesArray[0]);
+		for(var i=1; i<datesArray.length; i++){
+			var diff = Math.abs(dateToCompare-datesArray[i]);
+			if(diff < min){
+				index = i;
+			}
+		}
+		return index;
+	}	
+}
---
+++ yolo.js
@@ -0,0 +1,3 @@
+console.log("closest index to");
+var d = new date();
+console.log(d.closestToIndex(new Date(), [new Date(2015,0,1), new Date(2016, 0, 1), new Date(2014, 0, 1)]));
